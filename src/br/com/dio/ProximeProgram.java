02180aa0f015fdf3b40336f058a2ba975813a199
Fiama <fividal1@hotmail.com>
2022-04-28 13:39:37 -0300

    criando class Livro

--- src/br/com/dio/ProximeProgram.java
+++ src/br/com/dio/ProximeProgram.java
@@ -6,10 +6,44 @@ public class ProximeProgram {
     public static void main(String[] args) {
             Gato gato = new Gato();
         System.out.println(gato);
-        }
-            /*int a= 3;
-            int b = 2;
-        System.out.println("Hello World " + (a+b));*/
 
+        Livro livro22 = new Livro("A cabana", 500);
+        System.out.println(livro22);
+
+    }
+    }
+
+    class Livro{
+    private String nome;
+    private Integer numPagina;
+
+        public Livro(String nome, Integer numPagina) {
+            this.nome = nome;
+            this.numPagina = numPagina;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPagina() {
+            return numPagina;
+        }
+
+        public void setNumPagina(Integer numPagina) {
+            this.numPagina = numPagina;
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numPagina=" + numPagina +
+                    '}';
+        }
     }
 
